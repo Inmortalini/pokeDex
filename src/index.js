0880b6d1acc342ce5f01c019968b736f458c9a32
inmortalini <pedrobarrientos@gmail.com>
2023-03-18 16:11:47 -0400

    Agrego cambios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// async function pokeRender(){
+//     for(let i=0;i<=10;i++){
+//         let n=i+15
+//         const url = `https://pokeapi.co/api/v2/pokemon/${n}/`;
+//         const response= await fetch(url);
+//         data=await response.json()
+//         let element= document.createElement("div");
+//         element.innerText=`
+//         <p>${data.name}</p>
+//         <img src='${data.sprites.front_default}'></img>
+//         `;
+//         document.getElementById("central").appendChild(element);
+//     }
+// }
+// pokeRender()
 
-console.log('Happy hacking :)')
+async function pokeRender() {
+    try {
+      for (let i = 1; i <= 10; i++) {
+        let n = i + 15;
+        const url = `https://pokeapi.co/api/v2/pokemon/${n}/`;
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            let element = document.createElement("div");
+            element.innerHTML = `
+              <p>${data.name}</p>
+              <img src='${data.sprites.front_default}'></img>
+            `;
+            document.getElementById("central").appendChild(element);
+          })
+          .catch((err) => console.log(err));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  pokeRender();
